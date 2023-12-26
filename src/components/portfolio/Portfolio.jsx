5c2f220eb75da3ebcf1f437528272001f91ec8b5
Gundeti Srivardhan <113277469+srigundeti10@users.noreply.github.com>
2023-12-26 23:43:18 +0530

    portfolio done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Portfolio.css'
-import Img1 from '../../images/a.jpg'
-import Img2 from '../../images/b.jpg'
-import Img3 from '../../images/e.jpeg'
-import Img4 from '../../images/e.jpeg'
-import Img5 from '../../images/e.jpeg'
-import Img6 from '../../images/e.jpeg'
+import Img1 from '../../images/a.png'
+import Img2 from '../../images/b.png'
+import Img3 from '../../images/c.png'
+import Img4 from '../../images/d.jpg'
+import Img5 from '../../images/e.png'
+import Img6 from '../../images/f.png'
 
 const Portfolio = () => {
   return (
@@ -18,60 +18,77 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={Img1} alt="portfolio image 1" />
           </div>
-          <h3>Lamborghini</h3>
+          <h3>Smart Assistant</h3>
+          <br></br>
+          <h4> Nov. 2022 - Apr. 2023 </h4>
+          <br></br>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target='_blank'>Github</a>
-            <a href="https://github.com" className="btn btn-primary" target='_blank'>Demo</a>
+            <a href="https://drive.google.com/drive/folders/1ybX_2XOZhqdVBYsFds92V6H7tj896H2E?usp=sharing" className="btn" target='_blank'>Download Work</a>
+            <a href="https://github.com/srigundeti10/Smart-Assistant.git" className="btn btn-primary" target='_blank'>Github Demo</a>
           </div>
         </artical>
         <artical className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={Img2} alt="portfolio image 1" />
           </div>
-          <h3>Ferrari</h3>
+          <h3>Parallel Parking Problem</h3>
+          <br></br>
+          <h4> July. 2023 - Ongoing </h4>
+          <br></br>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target='_blank'>Github</a>
-            <a href="https://github.com" className="btn btn-primary" target='_blank'>Demo</a>
+            <a href="https://drive.google.com/drive/folders/1hzz_RuXsv5UkujIhGrOXnq4W0vHFlfPk?usp=sharing" className="btn" target='_blank'>Download Work</a>
+            <a href="https://github.com/srigundeti10/Parallel-Parking-Problem.git" className="btn btn-primary" target='_blank'>Github Demo</a>
           </div>
         </artical>
         <artical className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={Img3} alt="portfolio image 1" />
           </div>
-          <h3>stocks</h3>
+          <h3>CAM Profiling</h3>
+          <br></br>
+          <h4> April 2023 </h4>
+          <br></br>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target='_blank'>Github</a>
-            <a href="https://github.com" className="btn btn-primary" target='_blank'>Demo</a>
+            <a href="https://drive.google.com/drive/folders/18_jcss3RyddA4C_mQwzkb7-9GI4x5Tjg?usp=sharing" className="btn" target='_blank'>Download Work</a>
+            <a href="https://github.com/srigundeti10/CAM-Profiling.git" className="btn btn-primary" target='_blank'>Github Demo</a>
           </div>
         </artical>
         <artical className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={Img4} alt="portfolio image 1" />
           </div>
-          <h3>stocks</h3>
+          <h3>Analysis & Machining</h3>
+          <br></br>
+          <h4> Sep. 2023 - Nov. 2023 </h4>
+          <br></br>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target='_blank'>Github</a>
-            <a href="https://github.com" className="btn btn-primary" target='_blank'>Demo</a>
+            <a href="https://drive.google.com/drive/folders/1rePWHzAYnqnvTMAFgpL5QCi4t-iiFBxe?usp=sharing" className="btn" target='_blank'>Download Work</a>
+            <a href="https://github.com/srigundeti10/Analysis-Machining.git" className="btn btn-primary" target='_blank'>Github Demo</a>
           </div>
         </artical>
         <artical className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={Img5} alt="portfolio image 1" />
           </div>
-          <h3>stocks</h3>
+          <h3>Analysis of Conventional Sugar Mill Roller Shaft</h3>
+          <h4> Dec. 2023 - Ongoing </h4>
+          <br></br>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target='_blank'>Github</a>
-            <a href="https://github.com" className="btn btn-primary" target='_blank'>Demo</a>
+            {/* <a href="https://github.com" className="btn" target='_blank'>Download Work</a>
+            <a href="https://github.com" className="btn btn-primary" target='_blank'>Github Demo</a> */}
           </div>
         </artical>
         <artical className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={Img6} alt="portfolio image 1" />
           </div>
-          <h3>stocks</h3>
+          <h3>Simulation of Mechanisms</h3>
+          <br></br>
+          <h4> March 2022 </h4>
+          <br></br>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target='_blank'>Github</a>
-            <a href="https://github.com" className="btn btn-primary" target='_blank'>Demo</a>
+            <a href="https://drive.google.com/drive/folders/1p8cX89EBxco2XsC5Es459OwPvR-AClY0?usp=sharing" className="btn" target='_blank'>Download Work</a>
+            <a href="https://github.com/srigundeti10/Linkage-Project.git" className="btn btn-primary" target='_blank'>Github Demo</a>
           </div>
         </artical>
       </div>
