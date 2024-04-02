7773876034a91e884d03be80504f51f9725fd8c4
Gundeti Srivardhan <113277469+srigundeti10@users.noreply.github.com>
2024-04-02 16:53:46 +0530

    EV CV Updated final

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon"/>
               <h5>Experience</h5>
-              <small>Beginner</small>
+              <small>Intermediate</small>
             </article>
             <article className="about__card">
               <FiUsers className="about__icon"/>
