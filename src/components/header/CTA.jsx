ca5f74bf325400a42e862f179e42eb1e9411c3e9
Gundeti Srivardhan <113277469+srigundeti10@users.noreply.github.com>
2024-09-02 15:46:41 +0530

    M

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV1 from '../../images/CV_1.pdf'
+import CV1 from '../../images/CV1_pub_in.pdf'
 // import CV2 from '../../images/CV_FrontEnd.pdf'
 
 const CTA = () => {
